fd617ddbe68d50eaa57dc58bbc60eaac670381eb
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-11-08 10:41:44 -0300

    App configuration on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
 /**
  * Modules
- */
+*/
 
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import * as dotenv from "dotenv"
 
 /**
  * Variables
- */
+*/
+
+dotenv.config()
+
+if(!process.env.PORT) {
+    process.exit(1)
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10)
+
+const app = express()
 
 /**
  * Configuration
- */
+*/
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
 
 /**
  * Server
- */
+*/
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
