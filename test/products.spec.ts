785c14f8e10d399008f90245252fb6d5f425bb1d
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-11-08 17:19:18 -0300

    CRUD mongoose test setup

--- test/products.spec.ts
+++ test/products.spec.ts
@@ -22,3 +22,63 @@ describe('Criando documentos no MongoDB', () => {
         done();
     });
 });
+
+let product: any;
+
+// Criando um novo usuário
+product = new Product({
+    nome: 'March',
+    marca: 'Nissan',
+    vendedor: 'Roger',
+    faixaDePreco: 40.00
+});
+
+// Salvando o usuário
+product.save()
+
+// Testando a busca por usuário
+describe('Buscando usuário', () => {
+    it('Busca de um usuário pelo nome', (done) => {
+        Product.findOne({ name: 'March' })
+            .then((prod: any) => {
+                assert(prod.name === 'March');
+                done();
+            });
+            done();
+    });
+});
+
+// Testando o update de um usuário
+describe('Atualizando usuário', () => {
+    // Função Helper
+    function assertHelper(statement: any, done: any) {
+        statement
+         .then(() => Product.find({}))
+         .then((products: any) => {
+          assert(products.length === 1);
+          assert(products[0].name === 'March');
+          done();
+        });
+      }
+
+    it('Buscando e atualizando um usuário pelo nome', (done) => {
+        product.set('nome', 'Etios')
+        assertHelper(product.save(), done);
+        done();
+    });
+});
+
+// Testando a remoção de um usuário
+describe('Removendo usuário', () => {
+    it('Buscando e removendo um usuário pelo nome', (done) => {
+        Product.remove()
+        .then(() => Product.find({ name: 'Etios' }))
+            .then((product: any) => {
+                assert(product.name === null);
+                done();
+            });
+            done();
+    });
+});
+
+
